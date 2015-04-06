82a7fb8005673a9e00fa4bd4264165bf1f5e913d
Jorge Lucic <jvlucic@gmail.com>
2015-04-06 21:35:03 +0200

    Fusing angle admin

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -1,9 +1,43 @@
 'use strict';
 
 angular.module('adminApp', ['LocalStorageModule', 'tmh.dynamicLocale',
-    'ngResource', 'ui.router', 'ngCookies', 'pascalprecht.translate', 'ngCacheBuster', 'infinite-scroll'])
+    'ngResource', 'ui.router', 'ngCookies', 'pascalprecht.translate', 'ngCacheBuster', 'infinite-scroll',
+    'ngAnimate','ngStorage','ngCookies','ui.bootstrap', 'oc.lazyLoad','cfp.loadingBar','ngSanitize','ui.utils'])
 
-    .run(function ($rootScope, $location, $window, $http, $state, $translate, Auth, Principal, Language, ENV, VERSION) {
+    .run(function ($rootScope, $location, $window, $http, $state, $translate, Auth, Principal, Language, ENV, VERSION,$stateParams,$templateCache) {
+        /***ANGLE CONFIGURATION ***/
+        /*** J TODO: DELETE OR CHANGE THIS!!! ITS UGLY ******/
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        $rootScope.$storage = $window.localStorage;
+        /*** END J TODO: DELETE OR CHANGE THIS!!! ITS UGLY ******/
+
+        $rootScope.app = {
+            name: 'Angle',
+            description: 'Angular Bootstrap Admin Template',
+            year: ((new Date()).getFullYear()),
+            layout: {
+                isFixed: true,
+                isCollapsed: false,
+                isBoxed: false,
+                isRTL: false,
+                horizontal: false,
+                isFloat: false,
+                asideHover: false,
+                theme: null
+            },
+            useFullLayout: false,
+            hiddenFooter: false,
+            viewAnimation: 'ng-fadeInUp'
+        };
+        $rootScope.user = {
+            name:     'John',
+            job:      'ng-developer',
+            picture:  'app/img/user/02.jpg'
+        };
+
+        /***END ANGLE CONFIGURATION ***/
+
         $rootScope.ENV = ENV;
         $rootScope.VERSION = VERSION;
         $rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
@@ -45,7 +79,7 @@ angular.module('adminApp', ['LocalStorageModule', 'tmh.dynamicLocale',
             }
         };
     })
-    
+
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, $translateProvider, tmhDynamicLocaleProvider, httpRequestInterceptorCacheBusterProvider) {
 
         //enable CSRF
@@ -55,28 +89,57 @@ angular.module('adminApp', ['LocalStorageModule', 'tmh.dynamicLocale',
         //Cache everything except rest api requests
         httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/], true);
 
+/*
         $urlRouterProvider.otherwise('/');
-        $stateProvider.state('site', {
-            'abstract': true,
-            views: {
-                'navbar@': {
-                    templateUrl: 'scripts/components/navbar/navbar.html',
-                    controller: 'NavbarController'
-                }
-            },
-            resolve: {
-                authorize: ['Auth',
-                    function (Auth) {
-                        return Auth.authorize();
+*/
+        $urlRouterProvider.otherwise('/app/singleview');
+        $stateProvider
+            .state('site', {
+                'abstract': true,
+                views: {
+                    'navbar@': {
+                        templateUrl: 'scripts/components/navbar/navbar.html',
+                        controller: 'NavbarController'
                     }
-                ],
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('global');
-                    $translatePartialLoader.addPart('language');
-                    return $translate.refresh();
-                }]
-            }
-        });
+                },
+                resolve: {
+                    authorize: ['Auth',
+                        function (Auth) {
+                            return Auth.authorize();
+                        }
+                    ],
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('global');
+                        $translatePartialLoader.addPart('language');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'scripts/components/inventoryManagement/app.html',
+                controller: 'AppController',
+                data: {
+                    pageTitle: 'adminApp.authorizedEstablishmentUser.home.title'
+                }
+            })
+            .state('app.singleview', {
+                url: '/singleview',
+                title: 'Single View',
+                templateUrl: 'scripts/components/inventoryManagement/singleview.html',
+                data: {
+                    pageTitle: 'adminApp.authorizedEstablishmentUser.home.title'
+                }
+            })
+            .state('app.submenu', {
+                url: '/submenu',
+                title: 'Submenu',
+                templateUrl: 'scripts/components/inventoryManagement/submenu.html',
+                data: {
+                    pageTitle: 'adminApp.authorizedEstablishmentUser.home.title'
+                }
+            });
 
 
         // Initialize angular-translate
@@ -89,4 +152,23 @@ angular.module('adminApp', ['LocalStorageModule', 'tmh.dynamicLocale',
 
         tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
         tmhDynamicLocaleProvider.useCookieStorage('NG_TRANSLATE_LANG_KEY');
-    });
+    })
+    .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeBar = true;
+        cfpLoadingBarProvider.includeSpinner = false;
+        cfpLoadingBarProvider.latencyThreshold = 500;
+        cfpLoadingBarProvider.parentSelector = '.wrapper > section';
+    }])
+    .config(['$tooltipProvider', function ($tooltipProvider) {
+        $tooltipProvider.options({appendToBody: true});
+
+    }])
+    .config(['$ocLazyLoadProvider', 'APP_REQUIRES', function ($ocLazyLoadProvider, APP_REQUIRES) {
+        'use strict';
+        // Lazy Load modules configuration
+        $ocLazyLoadProvider.config({
+            debug: false,
+            events: true,
+            modules: APP_REQUIRES.modules
+        });
+    }]);
